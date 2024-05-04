611d3af0afa4f07ca13a7bc01cc636db4d02d7cb
Chrisxavix <chrischillogallid64@outlook.com>
2024-05-04 17:48:12 -0500

    search by id

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CountriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country.inferface';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-country-page',
   templateUrl: './country-page.component.html',
   styleUrl: './country-page.component.css'
 })
-export class CountryPageComponent {
+export class CountryPageComponent  implements OnInit {
+
+  public country?: Country;
+
+  constructor(
+    private activateRoute: ActivatedRoute,
+    private router: Router,
+    private countriesService: CountriesService,
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    this.activateRoute.params.
+    pipe(
+      /* Recibe parámetros */
+      switchMap( ({ id }) => this.countriesService.searchCountryByAlphaCode(id) )
+    )
+    .subscribe( ( country ) => {
+      if(!country) return this.router.navigateByUrl("");
+      console.log(country);
+      return  this.country = country;
+    })
+  }
 
 }
--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -15,6 +15,18 @@ export class CountriesService {
     private http: HttpClient,
   ) { }
 
+  searchCountryByAlphaCode(code: string): Observable<Country | null> {
+    const url = `${this.apiUrl}/alpha/${code}`;
+    return this.http.get<Country[]>(url)
+      .pipe(
+        map( countries => countries.length > 0 ? countries[0]: null ),
+        catchError( error => {
+          console.log("CONTROLADO" , error);
+          return of(null)
+        })
+      )
+  }
+
   searchCapital(term: string): Observable<Country[]> {
     const url = `${this.apiUrl}/capital/${term}`;
     return this.http.get<Country[]>(url)
@@ -37,8 +49,8 @@ export class CountriesService {
       )
   }
 
-  searchRegion(term: string): Observable<Country[]> {
-    const url = `${this.apiUrl}/region/${term}`;
+  searchRegion(region: string): Observable<Country[]> {
+    const url = `${this.apiUrl}/region/${region}`;
     return this.http.get<Country[]>(url)
       .pipe(
         catchError( error => {
